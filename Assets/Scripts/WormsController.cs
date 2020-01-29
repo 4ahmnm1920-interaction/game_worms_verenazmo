78a894180977d6410d40c404f905320ae237c333
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2020-01-29 11:30:32 +0100

    player controls

---
+++ Assets/Scripts/WormsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce;
+    public float moveForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 force = new Vector3(0, jumpForce, 0);
+            rb.AddForce(force);
+            Debug.Log("Space Taste ist gedrueckt");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 go = new Vector3(moveForce, 0, 0);
+            rb.AddForce(go);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 goback = new Vector3(-moveForce, 0, 0);
+            rb.AddForce(goback);
+        }
+    }
+
+}
